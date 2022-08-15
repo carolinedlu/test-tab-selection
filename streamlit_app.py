d1e1e76f0b30fc37a50e5af27c171358046c3612
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-08-15 17:36:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 with tab1:
-   st.write("Tab 1")
-with tab2:
-   st.write("Tab 2")
-with tab3:
-   st.write("Tab 3")
+#    st.write("Tab 1")
+# with tab2:
+#    st.write("Tab 2")
+# with tab3:
+#    st.write("Tab 3")
 
 if tab1:
    curr_tab = "Cat"
